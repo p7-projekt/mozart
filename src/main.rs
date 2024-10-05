4115c9e9eaa7c11eb49f59e828d3f60e4645906c
Andreas Krath <andreas.krath+github@gmail.com>
2024-10-05 23:57:22 +0200

    rm: clean slate

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,8 @@
-use axum::{routing::post, serve, Json, Router};
+use axum::{http::StatusCode, response::IntoResponse, routing::post, serve, Json, Router};
 use model::Task;
-use response::CheckResponse;
-use std::{
-    fs::{create_dir, read_to_string, remove_dir_all, File},
-    io::Write,
-    path::PathBuf,
-    process::Command,
-};
 use tokio::net::TcpListener;
-use uuid::Uuid;
 
 mod model;
-mod response;
 
 const HASKELL: &str = r###"
 main = do
@@ -50,129 +41,6 @@ async fn main() {
 // 7. read test case output from output file +
 // 8. clean up temporary task directory +
 // 9. construct task response +
-async fn check(Json(task): Json<Task>) -> CheckResponse {
-    let task_id = Uuid::new_v4();
-    let unique_temp_dir_path = PathBuf::from(format!("/tmp/{task_id}"));
-
-    if create_dir(unique_temp_dir_path.as_path()).is_err() {
-        return CheckResponse::Error;
-    }
-
-    let (solution, test_cases) = task.into_inner();
-    let haskell_test_cases = test_cases
-        .iter()
-        .map(|tc| tc.to_haskell_test_case())
-        .collect::<Box<[String]>>()
-        .join("\n");
-
-    let haskell_output_file_prefix = unique_temp_dir_path.to_string_lossy().to_string();
-    let mut haskell_code = HASKELL
-        .replace("UUID_PATH", haskell_output_file_prefix.as_str())
-        .replace("TEST_CASES", haskell_test_cases.as_str());
-    haskell_code.push_str(solution.as_str());
-
-    let test_file_path = PathBuf::from(format!("{}/Test.hs", unique_temp_dir_path.display()));
-    let mut test_file = match File::create_new(test_file_path.clone()) {
-        Ok(f) => f,
-        Err(_) => return CheckResponse::Error,
-    };
-
-    if test_file.write_all(haskell_code.as_bytes()).is_err() {
-        return CheckResponse::Error;
-    }
-
-    let executable_binary_path = PathBuf::from(format!("{}/test", unique_temp_dir_path.display()));
-    let executable_binary_path_string = executable_binary_path.to_string_lossy().to_string();
-    let test_file_path_string = test_file_path.to_string_lossy().to_string();
-    let compilation = Command::new("ghc")
-        .args([
-            "-O2",                                  // highest level of safe optimization
-            "-o",                                   // set output executable name
-            executable_binary_path_string.as_str(), // name of the output executeable
-            test_file_path_string.as_str(),         // the source program
-        ])
-        .output();
-
-    let compilation = match compilation {
-        Ok(o) => o,
-        Err(e) => return CheckResponse::Error,
-    };
-
-    let execution = Command::new(executable_binary_path).output();
-    let execution = match execution {
-        Ok(o) => o,
-        Err(e) => return CheckResponse::Error,
-    };
-
-    let test_case_results_file_path =
-        PathBuf::from(format!("{}/output", unique_temp_dir_path.display()));
-    let Ok(output_file_content) = read_to_string(test_case_results_file_path) else {
-        return CheckResponse::Error;
-    };
-
-    if let Err(err) = remove_dir_all(unique_temp_dir_path.as_path()) {
-        return CheckResponse::Error;
-    }
-
-    CheckResponse::Success
+async fn check(Json(task): Json<Task>) -> impl IntoResponse {
+    StatusCode::OK
 }
-
-// #[cfg(test)]
-// mod requests {
-//     use crate::app;
-//     use axum::http::{Method, Request, StatusCode};
-//     use tower::ServiceExt;
-
-//     #[tokio::test]
-//     async fn content_type_not_set_to_json() {
-//         let mozart = app();
-//         let request = Request::builder()
-//             .method(Method::POST)
-//             .uri("/check")
-//             .body(String::new())
-//             .expect("failed to create request");
-
-//         let response = mozart
-//             .oneshot(request)
-//             .await
-//             .expect("failed to oneshot request");
-
-//         assert_eq!(response.status(), StatusCode::UNSUPPORTED_MEDIA_TYPE);
-//     }
-
-//     #[tokio::test]
-//     async fn content_type_is_json_but_body_is_empty() {
-//         let mozart = app();
-//         let request = Request::builder()
-//             .method(Method::POST)
-//             .uri("/check")
-//             .header("Content-Type", "application/json")
-//             .body(String::new())
-//             .expect("failed to create request");
-
-//         let response = mozart
-//             .oneshot(request)
-//             .await
-//             .expect("failed to oneshot request");
-
-//         assert_eq!(response.status(), StatusCode::BAD_REQUEST);
-//     }
-
-//     #[tokio::test]
-//     async fn content_type_is_json_body_is_ill_formed() {
-//         let mozart = app();
-//         let request = Request::builder()
-//             .method(Method::POST)
-//             .uri("/check")
-//             .header("Content-Type", "application/json")
-//             .body(String::from("}"))
-//             .expect("failed to create request");
-
-//         let response = mozart
-//             .oneshot(request)
-//             .await
-//             .expect("failed to oneshot request");
-
-//         assert_eq!(response.status(), StatusCode::UNPROCESSABLE_ENTITY);
-//     }
-// }
