cc172c075abafca239b06d722ad66bf322d31b9e
Andreas Krath <andreas.krath+github@gmail.com>
2024-10-08 22:23:14 +0200

    feat: add error type

---
+++ src/error.rs
@@ -0,0 +1,14 @@
+use thiserror::Error;
+
+pub const UUID_SHOULD_BE_VALID_STR: &str = "a uuid should always be valid utf8 encoding";
+
+/// An error that occurs in relation to checking a solution.
+#[derive(Debug, Error)]
+pub enum CheckError {
+    /// This error is often in relation to creating, reading from or writing to files and directories.
+    #[error("an error occured during an IO interaction")]
+    IOInteraction,
+
+    #[error("an error occured during compilation: {0}")]
+    Compilation(String),
+}
