7046cc7a9d06e956b85c79246846faeea6232737
Andreas Krath <andreas.krath+github@gmail.com>
2024-10-13 02:02:10 +0200

    feat: add timeout_process function

---
+++ src/timeout.rs
@@ -0,0 +1,34 @@
+use crate::error::SubmissionError;
+use std::{
+    process::{Child, ExitStatus, Output},
+    time::Duration,
+};
+use tokio::time::{sleep, Instant};
+
+const GUARDED_EXPECT_DUE_TO_IF_CONDITION: &str = "guarded expect due to if condition";
+
+pub async fn timeout_process(
+    timeout: Duration,
+    mut process: Child,
+) -> Result<Option<(ExitStatus, Output)>, SubmissionError> {
+    let start = Instant::now();
+
+    while process.try_wait().is_ok_and(|es| es.is_none()) && start.elapsed() < timeout {
+        sleep(Duration::from_millis(100)).await;
+    }
+
+    match process.try_wait() {
+        Ok(Some(exit_status)) => {
+            let output = process
+                .wait_with_output()
+                .expect(GUARDED_EXPECT_DUE_TO_IF_CONDITION);
+            Ok(Some((exit_status, output)))
+        }
+        Ok(None) => {
+            process.kill().expect("should be able to kill child");
+            Ok(None)
+        }
+        // this is in a scenario where the process never started
+        Err(_) => Err(SubmissionError::IOInteraction),
+    }
+}
