30cee6b99c3aa024811c435ef468e4a885c0cd23
Andreas Krath <andreas.krath+github@gmail.com>
2024-10-18 14:10:47 +0200

    doc: add documentation for reason behind multiple mains

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,11 @@ mod timeout;
 /// The parent directory of all test runner jobs.
 const PARENT_DIR: &str = "/tmp";
 
+/// We need to initilize the logger before multithreading happens
+/// otherwise local time offset cannot be determined.
+///
+/// As a result we initialise the logger inside a synchronous main function,
+/// before calling the async tokio main function
 fn main() {
     log::init();
 
