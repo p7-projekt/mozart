9ddd7e922eaaf2d0ee87b394369afcaccd122183
Andreas Krath <andreas.krath+github@gmail.com>
2024-10-21 20:23:44 +0200

    doc: document logging functions

--- src/log.rs
+++ src/log.rs
@@ -2,8 +2,13 @@ use std::str::FromStr;
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::fmt::time::OffsetTime;
 
+/// The default log level applied if nothing else is specified.
 const DEFAULT_LOG_LEVEL: LevelFilter = LevelFilter::INFO;
 
+/// Initialises a global logging subscriber.
+///
+/// The only configuration is compile time based on the environment variable
+/// `MOZART_LOG` which will determine the log level enabled.
 pub fn init() {
     let level = level_filter(option_env!("MOZART_LOG"));
     let time = OffsetTime::local_rfc_3339().expect("could not initialize time offset");
@@ -21,6 +26,7 @@ pub fn init() {
         .expect("failed to initialize subscriber");
 }
 
+/// Determines the level filter based on the supplied optional string slice.
 fn level_filter(env_var: Option<&str>) -> LevelFilter {
     let Some(var) = env_var else {
         return DEFAULT_LOG_LEVEL;
