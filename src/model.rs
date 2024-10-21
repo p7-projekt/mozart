5ef0b409ed6aafeb7bd13132d09a215707438b03
Andreas Krath <andreas.krath+github@gmail.com>
2024-10-21 17:35:36 +0200

    rf: clean up use of serde tags
    prefers container tags to field tags - simplifies things, generally

--- src/model.rs
+++ src/model.rs
@@ -1,9 +1,9 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct Submission {
     pub solution: String,
-    #[serde(rename = "testCases")]
     pub test_cases: Box<[TestCase]>,
 }
 
@@ -14,60 +14,55 @@ impl Submission {
 }
 
 #[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct TestCase {
     pub id: u64,
-    #[serde(rename = "inputParameters")]
     pub input_parameters: Box<[Parameter]>,
-    #[serde(rename = "outputParameters")]
     pub output_parameters: Box<[Parameter]>,
 }
 
 #[derive(Deserialize, Serialize, PartialEq, Clone, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct Parameter {
-    #[serde(rename = "valueType")]
     pub value_type: String,
     pub value: String,
 }
 
 #[derive(Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct TestCaseResult {
     pub id: u64,
-    #[serde(rename = "testResult", flatten)]
+    #[serde(flatten)]
     pub test_result: TestResult,
 }
 
 #[derive(Serialize, PartialEq, Debug)]
-#[serde(tag = "testResult")]
+#[serde(rename_all = "camelCase", tag = "testResult")]
 pub enum TestResult {
     /// The test case passed.
-    #[serde(rename = "pass")]
     Pass,
 
     /// The result of the test case is unknown.
     ///
     /// This is likely caused by a previous test case causing a runtime error, thereby crashing the test runner.
-    #[serde(rename = "unknown")]
     Unknown,
 
     /// The test case did not pass.
-    #[serde(rename = "failure")]
     Failure(TestCaseFailureReason),
 }
 
 /// The reason why a given test case failed.
 #[derive(Serialize, PartialEq, Debug)]
-#[serde(tag = "cause", content = "details")]
+#[serde(rename_all = "camelCase", tag = "cause", content = "details")]
 pub enum TestCaseFailureReason {
     /// The answer to the test case was incorrect.
-    #[serde(rename = "wrongAnswer")]
+    #[serde(rename_all = "camelCase")]
     WrongAnswer {
-        #[serde(rename = "inputParameters")]
         input_parameters: Box<[Parameter]>,
         actual: String,
         expected: String,
     },
 
     /// A runtime error occured during the test case.
-    #[serde(rename = "runtimeError")]
     RuntimeError,
 }
