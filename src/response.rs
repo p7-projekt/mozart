64b21d61a33a2a7ca22fcbe2706847d3ad8f2f07
Andreas Krath <andreas.krath+github@gmail.com>
2024-10-22 22:38:48 +0200

    doc: document response module and objects

--- src/response.rs
+++ src/response.rs
@@ -1,3 +1,5 @@
+//! Contains objects in relation to how responses are produced based on how the submission check went.
+
 use crate::{error::SubmissionError, model::TestCaseResult};
 use axum::{
     http::StatusCode,
@@ -6,18 +8,35 @@ use axum::{
 };
 use serde::Serialize;
 
+/// A submission result indicates the result of checking a given submission.
+///
+/// This is an outward facing object, as it is serialized to JSON in the HTTP response for a given request.
 #[derive(Serialize)]
 #[serde(tag = "result", content = "reason")]
 pub enum SubmissionResult {
+    /// A submission successfully passed all test cases.
     #[serde(rename = "pass")]
     Pass,
 
+    /// A submission did not pass all test cases.
+    ///
+    /// The `Box<[TestCaseResult]>` should contain a slice of test case results,
+    /// both for passed and failed test cases. This way the frontend can
+    /// correctly identify which test cases failed, and why they failed.
     #[serde(rename = "failure")]
     Failure(Box<[TestCaseResult]>),
 
+    /// An error occured at some point during the check of the submission.
+    ///
+    /// This error is user facing, in that it represents errors that the user
+    /// is responsible for, such at compilation errors, timeouts and the like.
+    ///
+    /// The `String` is the underlying [`SubmissionError`] in string format.
     #[serde(rename = "error")]
     Error(String),
 
+    /// An internal error represents something that the user is not at fault for,
+    /// for example, not being able to spawn a compilation process, or creating a file.
     InternalError,
 }
 
