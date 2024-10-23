6cd2b37ce46cb0f28d61a858948994af862769c0
Andreas Krath <andreas.krath+github@gmail.com>
2024-10-23 20:06:48 +0200

    rf: remove endpoints namespace for testing

--- src/main.rs
+++ src/main.rs
@@ -104,68 +104,66 @@ async fn submit(Json(submission): Json<Submission>) -> SubmissionResult {
 }
 
 #[cfg(test)]
-mod endpoints {
-    mod status {
-        use crate::app;
-        use axum::{
-            body::{Body, HttpBody},
-            http::{request::Builder, Method, StatusCode},
-        };
-        use tower::ServiceExt;
+mod status {
+    use crate::app;
+    use axum::{
+        body::{Body, HttpBody},
+        http::{request::Builder, Method, StatusCode},
+    };
+    use tower::ServiceExt;
 
-        #[tokio::test]
-        async fn invalid_http_method() {
-            let mozart = app();
-            let expected_status_code = StatusCode::METHOD_NOT_ALLOWED;
-            let request = Builder::new()
-                .method(Method::POST)
-                .uri("/status")
-                .body(Body::empty())
-                .expect("failed to build request");
+    #[tokio::test]
+    async fn invalid_http_method() {
+        let mozart = app();
+        let expected_status_code = StatusCode::METHOD_NOT_ALLOWED;
+        let request = Builder::new()
+            .method(Method::POST)
+            .uri("/status")
+            .body(Body::empty())
+            .expect("failed to build request");
 
-            let actual = mozart
-                .oneshot(request)
-                .await
-                .expect("failed to await oneshot");
+        let actual = mozart
+            .oneshot(request)
+            .await
+            .expect("failed to await oneshot");
 
-            assert_eq!(actual.status(), expected_status_code);
-        }
+        assert_eq!(actual.status(), expected_status_code);
+    }
 
-        #[tokio::test]
-        async fn body_content_does_not_affect_request() {
-            let mozart = app();
-            let expected_status_code = StatusCode::OK;
-            let request = Builder::new()
-                .method(Method::GET)
-                .uri("/status")
-                .body(Body::from("Hello, world!"))
-                .expect("failed to build request");
+    #[tokio::test]
+    async fn body_content_does_not_affect_request() {
+        let mozart = app();
+        let expected_status_code = StatusCode::OK;
+        let request = Builder::new()
+            .method(Method::GET)
+            .uri("/status")
+            .body(Body::from("Hello, world!"))
+            .expect("failed to build request");
 
-            let actual = mozart
-                .oneshot(request)
-                .await
-                .expect("failed to await oneshot");
+        let actual = mozart
+            .oneshot(request)
+            .await
+            .expect("failed to await oneshot");
 
-            assert_eq!(actual.status(), expected_status_code);
-        }
+        assert_eq!(actual.status(), expected_status_code);
+    }
 
-        #[tokio::test]
-        async fn valid() {
-            let mozart = app();
-            let expected_status_code = StatusCode::OK;
-            let request = Builder::new()
-                .method(Method::GET)
-                .uri("/status")
-                .body(Body::empty())
-                .expect("failed to build request");
+    #[tokio::test]
+    async fn valid() {
+        let mozart = app();
+        let expected_status_code = StatusCode::OK;
+        let request = Builder::new()
+            .method(Method::GET)
+            .uri("/status")
+            .body(Body::empty())
+            .expect("failed to build request");
 
-            let actual = mozart
-                .oneshot(request)
-                .await
-                .expect("failed to await oneshot");
+        let actual = mozart
+            .oneshot(request)
+            .await
+            .expect("failed to await oneshot");
 
-            assert_eq!(actual.status(), expected_status_code);
-            assert!(actual.body().is_end_stream());
-        }
+        assert_eq!(actual.status(), expected_status_code);
+        assert!(actual.body().is_end_stream());
     }
 }
