dca632dc4ed684e04d998fc81e5102cf650894c7
Andreas Krath <andreas.krath+github@gmail.com>
2024-10-24 11:51:49 +0200

    rf: mark integration test as done

--- src/main.rs
+++ src/main.rs
@@ -175,8 +175,8 @@ mod haskell {
     // - all test cases wrong answer (for all data types)
     // - runtime error in no last test case
     // - solution with multiple different data types as input (possibly one with all different data types, like a string creation exercise or something) +
-    // - solution with multiple different data types as output
     // - compilation error
+    // - solution with multiple different data types as output +
     // - timeout error on execution (i think compilation is impossible to check unfortunately)
     // - mix of pass and fail test cases + runtime error at some point
 
